14ae2e73ff69f022fae39be113860408a4c756f8
Arnaud <abracchetti@gmail.com>
2017-08-15 19:03:30 +0200

    Amélioration de la gestion des style.
    - Mise en place des styles au format SCSS
    - Définition d'un theme pirmeNG propre au projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {MenuItem} from 'primeng/primeng';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     title = 'app';
